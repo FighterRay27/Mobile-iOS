40da06a204f6b5399c843000cdea47e7d25506da
user <495871401>
2015-12-10 12:30:28 +0800

    widget 解析数据

--- MoblieCQUPT_iOS/ClassWidget/TodayViewController.m
+++ MoblieCQUPT_iOS/ClassWidget/TodayViewController.m
@@ -9,24 +9,24 @@
 #define kAPPGroupID @"group.com.mredrock.cyxbs"
 #define kAppGroupShareNowDay @"nowDay"
 #define kAppGroupShareThisWeekArray @"thisWeekArray"
-
+#define kAutoUpdateInterval 60*5
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
 
 @interface TodayViewController () <NCWidgetProviding>
-
+@property (strong, nonatomic) NSArray *todayClassArray;
 @end
 
 @implementation TodayViewController
 
+- (void)viewWillAppear:(BOOL)animated{
+    [self autoUpdateTimer];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSUserDefaults *shared = [[NSUserDefaults alloc] initWithSuiteName:kAPPGroupID];
-    NSString *nowWeek = [shared stringForKey:kAppGroupShareNowDay];
-    NSArray *weakDataArray = [shared objectForKey:kAppGroupShareThisWeekArray];
-//    [shared synchronize];
-    NSLog(@"共享数据%@:%@",nowWeek,weakDataArray);
+    
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -46,4 +46,49 @@
     completionHandler(NCUpdateResultNewData);
 }
 
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets{
+    return UIEdgeInsetsMake(0, 0, 0, 0);
+}
+
+- (NSArray *)todayClassArrayFromWeakClassArray:(NSArray *)weakClassArray{
+    NSDateComponents *componets = [[NSCalendar autoupdatingCurrentCalendar] components:NSCalendarUnitWeekday fromDate:[NSDate date]];
+    NSInteger today =  [componets weekday];
+    today -= 1; //从周日开始转为从周一开始
+    today = today>0?today:7;
+    NSLog(@"%@",[NSDate date]);
+    
+    NSMutableArray *mutableToDayClassArray = [NSMutableArray array];
+    for (NSDictionary *row in weakClassArray) {
+        NSString *tmpString = row[@"day"];
+        if ([tmpString integerValue] == today) {
+            [mutableToDayClassArray addObject:row];
+        }
+    }
+    
+    NSLog(@"今日周%ld,数据:%@",today,mutableToDayClassArray);
+    return mutableToDayClassArray;
+}
+
+- (void)autoUpdateTimer{
+    NSTimer *timer = [[NSTimer alloc]
+                initWithFireDate:[NSDate distantPast]
+                        interval:kAutoUpdateInterval
+                        target:self
+                        selector:@selector(updateTodayClass)
+                        userInfo:nil
+                        repeats:YES];
+    
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+    
+}
+
+- (void)updateTodayClass{
+    
+    [self widgetPerformUpdateWithCompletionHandler:^(NCUpdateResult result) {
+        NSUserDefaults *shared = [[NSUserDefaults alloc] initWithSuiteName:kAPPGroupID];
+        NSArray *weakDataArray = [shared objectForKey:kAppGroupShareThisWeekArray];
+        self.todayClassArray = [self todayClassArrayFromWeakClassArray:weakDataArray];
+    }];
+    NSLog(@"更新今日课程数据");
+}
 @end
